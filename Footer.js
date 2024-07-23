c08ae419caa12b1b0460f3d0c72b60c303777154
walkerbunch <112009340+walkerbunch@users.noreply.github.com>
2024-07-23 00:04:44 -0400

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <nav className="footer">
-      <a href="https://github.com/walkerbunch/Project-4-Web-App-Dev/tree/main">GitHub</a>
+      <a href="https://github.com/walkerbunch/Project-4-Web-App-Dev/tree/main" target="_blank">GitHub</a>
     </nav>
   );
 }
